d536161e44503f24264f5eda94f92ad532099230
Ayush Gupta <ayushgupta1925@gmail.com>
2025-09-26 18:46:13 +0530

    Update multer.js

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -3,14 +3,16 @@ import path from "path";
 import fs from "fs";
 
 
-const storage = multer.diskStorage({
-  destination:(req, file, cb) =>{
-    cb(null, "./public");
-  },
-  filename:  (req, file, cb)=> {
-    cb(null, file.originalname);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination:(req, file, cb) =>{
+//     cb(null, "./public");
+//   },
+//   filename:  (req, file, cb)=> {
+//     cb(null, file.originalname);
+//   }
+// });
+
+const storage = multer.memoryStorage(); 
 
 const upload = multer({ storage });
 export default upload;
