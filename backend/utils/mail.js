7763eb148d48bcb6f314acd8bf7044bc963e4e2c
Ayush Gupta <ayushgupta1925@gmail.com>
2025-09-27 16:32:26 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-
 dotenv.config();
+import axios from "axios";
 
 const transporter = nodemailer.createTransport({
    host: "smtp-relay.sendinblue.com",
